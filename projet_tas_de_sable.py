8c27f6383c12246fcc6ef5d2df85fc761846bb08
Clément MARCHAL <clement.noisy78@gmail.com>
2022-02-11 11:32:11 +0100

    update 1

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -6,3 +6,49 @@
 # MARCHAL Clément
 # https://github.com/clement3872/Project_sandpiles.git
 ###################################
+
+
+# importation des lib
+import tkinter as tk
+import random
+
+# Listes principales avec toutes les piles de sablese (complétés plus tard)
+L_SANDPILES = [
+    ["","#","#","#",""],
+    ["#","#"],
+    ["#","#"],
+    ["#","#"],
+    ["","#","#","#",""]
+]
+
+WIDTH_TERRAIN = 3
+
+#est-ce qu'il faut faire ça ?
+def stability_test(l):
+    count = 0
+    for y in l:
+        for x in y:
+            if x >3: count +=1
+
+    return count >= 5
+
+def create_table(l_sandpiles, width_terrain=3):
+    # Créer une liste qui contient toutes les cases
+    for y in range(1,len(l_sandpiles)-1):
+        for x in range(width_terrain):
+            l_sandpiles[y].insert(1,random.randint(0,3))
+    
+    return l_sandpiles
+
+
+
+
+
+
+L_SANDPILES = create_table(L_SANDPILES)
+
+#Juste afficher dans la console, pour tester
+def display_list(l):
+    for el in l:
+        print(el)
+display_list(L_SANDPILES)
