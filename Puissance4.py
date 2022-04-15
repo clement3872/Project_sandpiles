0b1505b34fdbf2a65da0bb4384575e28a403e92e
Clément MARCHAL <clement.noisy78@gmail.com>
2022-04-15 10:42:44 +0200

    update1

--- Puissance4.py
+++ Puissance4.py
@@ -7,3 +7,24 @@
 # https://github.com/clement3872/Puissance4.git
 ###################################
 
+import tkinter as tk
+
+
+def create_token(x,y,color="white", outline_=""):
+    global canvas
+    x0,y0, x1,y1 = x+BORDER,y+BORDER, x+radius+BORDER, y+radius+BORDER
+    canvas.create_oval(x0,y0, x1,y1, fill=color, outline="blue")
+
+
+radius = 75
+BORDER = 20
+WITDH,HEIGHT = 7*radius + BORDER, 6*radius + BORDER
+
+
+root = tk.Tk()
+canvas = tk.Canvas(root,bg="blue", width=WITDH, height=HEIGHT)
+canvas.pack()
+
+create_token(0,0)
+
+root.mainloop()
